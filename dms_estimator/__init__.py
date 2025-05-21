c0599c01a8fb74b3aee54073592cf5448474b4c1
Li-Jesse-Jiaze <robustlijiaze@gmail.com>
2025-05-21 21:46:14 +0200

    [REF] rename file

--- dms_estimator/__init__.py
+++ dms_estimator/__init__.py
@@ -1,4 +1,4 @@
 from dms_estimator.parameter_estimator import ParameterEstimator  # noqa
-from dms_estimator.interface import *  # noqa
+from dms_estimator.examples import *  # noqa
 
 __module_name__ = __name__
